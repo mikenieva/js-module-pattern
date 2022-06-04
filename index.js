9441dff094469dc599299d77e4a56bc8566d21cb
Mike Nieva <m@mikenieva.com>
2022-06-04 10:54:00 -0500

    Created calculator.js

---
+++ index.js
@@ -0,0 +1,39 @@
+// ./index.js
+
+// 1. IMPORTACIONES
+import { sum } from './lib/calculadora.js'
+
+// 2. VARIABLES
+const getResult = document.querySelector("#get-result")
+const result = document.querySelector("#result")
+
+
+// 3. EVENTOS
+
+getResult.addEventListener("click", (event) => {
+
+  // DETENER LA RECARGA DE PÁGINA POR DEFECTO
+  event.preventDefault()
+
+  // ABSTRAIGA LOS DATOS DE LAS VARIABLES
+  
+  // A. OBTENER DATO DE FORMULARIO
+  const quantityOne = form["quantity-one"].value
+  // B. CONVERTIR A "INTEGER"
+  const parsedQuantityOne = parseInt(quantityOne)
+
+  const quantityTwo = form["quantity-two"].value
+  const parsedQuantityTwo = parseInt(quantityTwo)
+
+  
+  const resultSum = sum(parsedQuantityOne, parsedQuantityTwo)
+
+  result.innerHTML = resultSum
+
+  return 
+
+})
+
+
+
+
