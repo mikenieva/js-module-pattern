b5c73495085c7273cfb4a2dabdfa52e880a4913a
Mike Nieva <m@mikenieva.com>
2022-06-11 10:39:19 -0500

    Registered calculator.js

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 // ./index.js
 
 // 1. IMPORTACIONES
-import { sum } from './lib/calculadora.js'
+// import { sum } from "./lib/calculadora.js";
+
+import { sum } from "./lib/calculadora.js";
 
 // 2. VARIABLES
-const getResult = document.querySelector("#get-result")
-const result = document.querySelector("#result")
-
+const getResult = document.querySelector("#get-result");
+const result = document.querySelector("#result");
+const form = document.querySelector("#form");
 
 // 3. EVENTOS
 
 getResult.addEventListener("click", (event) => {
-
   // DETENER LA RECARGA DE PÁGINA POR DEFECTO
-  event.preventDefault()
+  event.preventDefault();
 
   // ABSTRAIGA LOS DATOS DE LAS VARIABLES
-  
+
   // A. OBTENER DATO DE FORMULARIO
-  const quantityOne = form["quantity-one"].value
+  const quantityOne = form["quantity-one"].value;
   // B. CONVERTIR A "INTEGER"
-  const parsedQuantityOne = parseInt(quantityOne)
+  const parsedQuantityOne = parseInt(quantityOne);
 
-  const quantityTwo = form["quantity-two"].value
-  const parsedQuantityTwo = parseInt(quantityTwo)
+  const quantityTwo = form["quantity-two"].value;
+  const parsedQuantityTwo = parseInt(quantityTwo);
 
-  
-  const resultSum = sum(parsedQuantityOne, parsedQuantityTwo)
+  const resultSum = sum(parsedQuantityOne, parsedQuantityTwo);
 
-  result.innerHTML = resultSum
+  result.innerHTML = resultSum;
 
-  return 
-
-})
-
-
-
-
+  return;
+});
